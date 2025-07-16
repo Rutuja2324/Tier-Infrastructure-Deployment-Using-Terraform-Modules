43881d9b925f33455ea2efad76e93ff15421fad0
Rutuja2324 <rutukakuste24@gmail.com>
2025-07-16 14:55:55 +0530

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -1,1 +1,20 @@
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "db-subnet-group"
+  subnet_ids = var.subnet_ids
 
+  tags = {
+    Name = "rds-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "mydb" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  db_name              = "mydatabase"
+  username             = var.db_username
+  password             = var.db_password
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
+  skip_final_snapshot  = true
+}
